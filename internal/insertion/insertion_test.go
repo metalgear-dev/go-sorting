c6ced07d6b521fc1bd436404b536f96f6d9b24ef
metalgear-dev <dantes0121@hotmail.com>
2023-10-16 22:06:34 +0800

    feat: add insertion test

--- internal/insertion/insertion_test.go
+++ internal/insertion/insertion_test.go
@@ -3,6 +3,7 @@ package insertion
 import (
 	"math/rand"
 	"sort"
+	"sync"
 	"testing"
 	"time"
 
@@ -10,12 +11,36 @@ import (
 )
 
 func TestInsertionSort(t *testing.T) {
-	for n := 0; n < 10; n++ {
+	resChannel := make(chan bool)
+	var wg sync.WaitGroup
+
+	execSort := func(wg *sync.WaitGroup) {
+		defer wg.Done()
+
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		temp := r.Perm(100000)
 		InsertionSort(temp)
-		assert.True(t, sort.IntsAreSorted(temp))
+		resChannel <- sort.IntsAreSorted(temp)
 	}
+
+	checkResult := func() {
+		for res := range resChannel {
+			assert.True(t, res)
+		}
+	}
+
+	wg.Add(10)
+
+	// start sorting asynchronously
+	for n := 0; n < 10; n += 1 {
+		go execSort(&wg)
+	}
+
+	// check result
+	go checkResult()
+
+	wg.Wait()
+	defer close(resChannel)
 }
 
 func BenchmarkInsertionSort(b *testing.B) {
